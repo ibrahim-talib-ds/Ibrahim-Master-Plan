af390f5ffa3d04f1dd1197243e6c1ea52bf72c05
ibrahim-talib-ds <ibrahimutalibu@gmail.com>
2026-09-22 20:07:28 +0500

    Add 3 languages: EN/SW/TR

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Master Plan Service Worker
-const CACHE = 'mp-v22-' + Date.now();
+const CACHE = 'mp-v23-' + Date.now() + Date.now();
 const ASSETS = ['/', '/index.html'];
 
 self.addEventListener('install', e => {
