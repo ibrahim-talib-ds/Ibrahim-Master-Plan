cacb626db674942f84f64a232a067c6a73a7d490
ibrahim-talib-ds <ibrahimutalibu@gmail.com>
2026-09-22 20:29:23 +0500

    Fix Share Image button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Master Plan Service Worker
-const CACHE = 'mp-1790090255' + Date.now() + Date.now();
+const CACHE = 'mp-1790090952' + Date.now() + Date.now();
 const ASSETS = ['/', '/index.html'];
 
 self.addEventListener('install', e => {
