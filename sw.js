0dbb5c5bfe4bd4e7fd0d719858ee6f65d365be69
ibrahim-talib-ds <ibrahimutalibu@gmail.com>
2026-09-24 07:29:33 +0500

    Fix 7 bugs: BOOK_LIBRARY, tabs, cache, DAILY_READING

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Master Plan Service Worker
-const CACHE = 'mp-1790216736' + Date.now() + Date.now();
+const CACHE = 'mp-1790216954' + Date.now() + Date.now();
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
